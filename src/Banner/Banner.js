a6de872b7d0e4b7114fc874d6ae767dc34801dc3
Yyamaguchiosamu1997 <yshankoudao1222@gmail.com>
2020-12-14 08:51:19 +0900

    Add Banner, Navbar fixed position2

---
+++ src/Banner/Banner.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Banner = styled.div`
+height: 200px;
+background-image: url(img/banner.jpeg);
+background-size: cover;
+background-position: center;
+
+`
