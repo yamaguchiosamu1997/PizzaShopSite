53c836a6f65a90184d91879957ef1c48dde56342
Yyamaguchiosamu1997 <yshankoudao1222@gmail.com>
2020-12-14 11:18:13 +0900

    Add Menu, Food data and FoodGrid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { Navbar } from "./Navbar/Navbar";
 import { Banner } from "./Banner/Banner";
+import { Menu } from "./Menu/Menu";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -21,6 +22,7 @@ function App() {
       <GlobalStyle />
       <Navbar />
       <Banner />
+      <Menu />
       <div>Hello</div>
     </>
   );
---
+++ src/Data/FoodData.js
@@ -0,0 +1,31 @@
+export const foods = [
+    {
+        name: "Chesse Pizza",
+        img: "/img/pizza.png"
+    },
+    {
+        name: "Pepperoni Pizza",
+        img: "/img/pizza2.jpeg",
+    },
+    {
+        name: "Chicken Pizza",
+        img: "/img/chicken-pizza.jpeg",
+    },
+    {
+        img: "/img/healthy-pizza.jpeg",
+        name: "Veggie Pizza",
+    },
+    {
+        img: "/img/burger.jpeg",
+        name: "Burger",
+    },
+    { img: "/img/gyro.jpeg", name: "Gyro", },
+    {
+        img: "/img/sandwich.jpeg",
+        name: "Shrimp PoBoy",
+    },
+    {
+        img: "/img/fries.jpeg",
+        name: "Fries",
+    }
+];
