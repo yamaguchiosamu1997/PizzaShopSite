2b0401f38d7072aa0f075478ac4e67ad2229a618
Yyamaguchiosamu1997 <yshankoudao1222@gmail.com>
2020-12-14 15:15:13 +0900

    Food Sections

--- src/Data/FoodData.js
+++ src/Data/FoodData.js
@@ -1,31 +1,46 @@
-export const foods = [
+export const foodItems = [
     {
         name: "Chesse Pizza",
-        img: "/img/pizza.png"
+        img: "/img/pizza.png",
+        section: "Pizza"
     },
     {
         name: "Pepperoni Pizza",
         img: "/img/pizza2.jpeg",
+        section: "Pizza",
     },
     {
         name: "Chicken Pizza",
         img: "/img/chicken-pizza.jpeg",
+        section: "Pizza",
     },
     {
         img: "/img/healthy-pizza.jpeg",
         name: "Veggie Pizza",
+        section: "Pizza",
     },
     {
         img: "/img/burger.jpeg",
         name: "Burger",
+        section: "Sandwich",
     },
-    { img: "/img/gyro.jpeg", name: "Gyro", },
+    { img: "/img/gyro.jpeg", name: "Gyro", section: "Sandwich"},
     {
         img: "/img/sandwich.jpeg",
         name: "Shrimp PoBoy",
+        section: "Sandwich",
     },
     {
         img: "/img/fries.jpeg",
         name: "Fries",
+        section: "Sides",
     }
 ];
+
+export const foods = foodItems.reduce((res, food) => {
+    if (!res[food.section]) {
+        res[food.section] = [];
+    }
+    res[food.section].push(food);
+    return res;
+}, {});
--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -9,16 +9,20 @@ margin: 0px 400px 50px 20px;
 `
 
 export function Menu() {
-    return <MenuStyled>
-        <h1> Menu </h1>
-        <FoodGrid>
-            {foods.map(food => (
-                <Food img={food.img}>
-                    <FoodLabel>
-                        {food.name}
-                    </FoodLabel>    
-                </Food >
+    return (
+        <MenuStyled>
+            {Object.entries(foods).map(([sectionName, foods]) => (
+                <>
+                    <h1> {sectionName} </h1>
+                    <FoodGrid>
+                        {foods.map(food => (
+                            <Food img={food.img}>
+                                <FoodLabel>{food.name}</FoodLabel>
+                            </Food >
+                        ))}
+                    </FoodGrid>
+                </>
             ))}
-        </FoodGrid>
-    </MenuStyled>;
+    </MenuStyled>
+    );
 }
