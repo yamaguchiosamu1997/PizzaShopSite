cbb49159ddde5470e51f00f305840440006aa21a
Yyamaguchiosamu1997 <yshankoudao1222@gmail.com>
2020-12-15 14:03:01 +0900

    Add Order and FoodDialogFooter

--- src/FoodDialog/FoodDialog.js
+++ src/FoodDialog/FoodDialog.js
@@ -1,17 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 import { FoodLabel } from "../Menu/FoodGrid";
+import { pizzaRed } from "../Styles/colors";
+import { Title } from "../Styles/title";
 
 const Dialog = styled.div`
 width: 500px;
-height: 2000px;
 background-color: white;
 position: fixed;
 top: 75px;
 z-index: 5;
 max-height: calc(100% - 100px);
 left: calc(50% - 250px);
+display: flex;
+flex-direction: column;
 `;
+
+export const DialogContent = styled.div`
+overflow: auto;
+min-height: 100px;
+`;
+
+export const DialogFooter = styled.div`
+box-shadow: 0px -2px 10px 0px grey;
+height: 60px;
+display: flex;
+justify-content: center;
+`;
+
+export const ConfirmButton = styled(Title)`
+margin: 10px;
+color: white;
+height: 20px;
+border-radius: 5px;
+padding: 10px;
+text-align: center;
+width: 200px;
+cursor: pointer;
+background-color: ${pizzaRed};
+`
 
 const DialogShadow = styled.div`
 position: fixed;
@@ -43,15 +70,17 @@ export function FoodDialog({ openFood, setOpenFood }) {
     }
     if (!openFood) return null;
     return (
-        openFood ? (
             <>
                 <DialogShadow onClick={close}/>
                 <Dialog>
                     <DialogBanner img={openFood.img}>
                         <DialogBannerName> { openFood.name }</DialogBannerName>
                     </DialogBanner>
+                    <DialogContent />
+                    <DialogFooter>
+                        <ConfirmButton>Add to order</ConfirmButton>
+                    </DialogFooter>
                 </Dialog>
             </>
-        ) : null
     );
 }
