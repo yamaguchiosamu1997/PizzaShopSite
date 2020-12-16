9bf4de612508064cc2c73188e140286c7674d0d4
Yyamaguchiosamu1997 <yshankoudao1222@gmail.com>
2020-12-16 09:28:24 +0900

    Add Favicon and UseTitlle

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ import { GlobalStyle } from "./Styles/GlobalStyle";
 import { Order } from './Order/Order';
 import { useOpenFood } from "./Hooks/useOpenFood";
 import { useOrders } from "./Hooks/useOrders";
+import { useTitle } from "./Hooks/useTitle";
 
 
 function App() {
   const openFood = useOpenFood();
   const orders = useOrders();
+  useTitle({ ...openFood, ...orders });
 
   return (
     <>
